144d505d45e986615a478c0701ac41f8c0751390
Hung Tran Manh <tranmanhhung@tma.com.vn>
2019-09-09 15:17:15 +0700

    examples of PassByValue for ReferenceType and ValueType

--- Grades.Tests/TypeTests.cs
+++ Grades.Tests/TypeTests.cs
@@ -6,8 +6,37 @@ using NUnit.Framework.Internal;
 namespace Grades.Tests
 {
     [TestFixture]
-    public class ReferenceTypeTests
+    public class TypeTests
     {
+        [Test]
+        public void ReferenceTypesPassByValue()
+        {
+            GradeBook book1 = new GradeBook();
+            GradeBook book2 = book1;
+            
+            GiveBookAName(book2);
+            Assert.AreEqual("A GradeBook", book1.Name);
+        }
+
+        private void GiveBookAName(GradeBook book)
+        {
+            book.Name = "A GradeBook";
+        }
+        
+        [Test]
+        public void ValueTypesPassByValue()
+        {
+            int x = 100;
+            IncrementNumber(x);
+            Assert.AreNotEqual(101, x);
+            Assert.AreEqual(100, x);
+        }
+
+        private void IncrementNumber(int num)
+        {
+            num += 1;
+        }
+        
         [Test]
         public void StringComparasions()
         {
