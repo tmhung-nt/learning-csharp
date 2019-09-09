2918a69b3fb8b04d2810023cb25a7c981497ac7d
Hung Tran Manh <tranmanhhung@tma.com.vn>
2019-09-09 14:28:06 +0700

    GradeBook classes and Unit Tests

---
+++ Grades.Tests/GradeBookTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+namespace Grades.Tests
+{
+    [TestFixture]
+    public class GradeBookTests
+    {
+        [Test]
+        public void ComputeHighestGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(10);
+            book.AddGrade(90);
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual(90, result.HighestGrade);
+        }
+        
+        [Test]
+        public void ComputeLowestGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(75);
+            book.AddGrade(90);
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual(75, result.LowestGrade);
+        }
+        
+        [Test]
+        public void ComputeAverageGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(75);
+            book.AddGrade(89.5f);
+            book.AddGrade(91);
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual(85.16, result.AverageGrade, 0.01);
+        }
+    }
+}
