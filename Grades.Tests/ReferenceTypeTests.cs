b5cdaa1076953c7b42016972a8692d53eab8eb2f
Hung Tran Manh <tranmanhhung@tma.com.vn>
2019-09-09 15:08:09 +0700

    example of using enum

--- Grades.Tests/ReferenceTypeTests.cs
+++ Grades.Tests/ReferenceTypeTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading;
 using NUnit.Framework;
+using NUnit.Framework.Internal;
 
 namespace Grades.Tests
 {
@@ -8,13 +9,32 @@ namespace Grades.Tests
     public class ReferenceTypeTests
     {
         [Test]
-        public void VariablesHoldAReference()
+        public void StringComparasions()
+        {
+            string name1 = "Hung";
+            string name2 = "hung";
+
+            bool result = String.Equals(name1, name2, StringComparison.InvariantCultureIgnoreCase);
+            Assert.IsTrue(result);
+        }
+        [Test]
+        public void GradeBookVariablesHoldAReference()
         {
             GradeBook g1 = new GradeBook();
             GradeBook g2 = g1;
 
             g1.Name = "Hung's book";
             Assert.AreEqual(g2.Name, g1.Name);
+        }
+
+        [Test]
+        public void IntVariableHoldAValue()
+        {
+            int x1 = 100;
+            int x2 = x1;
+
+            x1 += 2;
+            Assert.AreNotEqual(x1, x2);
         }
     }
 }
