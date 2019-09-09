10d40b3bbdccb5089c470798f6229bffbbc8f654
Hung Tran Manh <tranmanhhung@tma.com.vn>
2019-09-09 16:37:45 +0700

    writeLine using template (formatting)

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -6,7 +6,25 @@ namespace Grades
     {
         public static void Main(string[] args)
         {
+            GradeBook book = new GradeBook();
+            book.AddGrade(88);
+            book.AddGrade(99);
+            book.AddGrade(93);
+            GradeStatistics stats = book.ComputeStatistics();
+            WriteResult("Average", stats.AverageGrade);
+            WriteResult("Highest", (int)stats.HighestGrade);
+            WriteResult("Lowest", stats.LowestGrade);
+        }
 
+        static void WriteResult(string description, float result)
+        {
+//            Console.WriteLine("{0}: {1:C}", description, result);
+            Console.WriteLine($"{description}: {result}");
+        }
+        
+        static void WriteResult(string description, int result)
+        {
+            Console.WriteLine(description + ": " + result);
         }
     }
 }
