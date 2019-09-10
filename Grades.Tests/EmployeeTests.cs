2558d34c023fdcb5c2576302e88cafeb93641d18
Hung Tran Manh <tranmanhhung@tma.com.vn>
2019-09-10 10:33:21 +0700

    delegate example

---
+++ Grades.Tests/EmployeeTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace Grades.Tests
+{
+    [TestFixture]
+    public class TestEmployee
+    {
+        [Test]
+        public void TestNumberOfEmployees()
+        {
+            Employee.NumberOfEmployees = 107;
+            Employee e1 = new Employee();
+            e1.Name = "Claude Vige";
+
+            Assert.AreEqual(108, Employee.NumberOfEmployees);
+            Assert.AreEqual("Claude Vige", e1.Name);
+        }
+        /* Output:
+            Employee number: 108
+            Employee name: Claude Vige
+        */
+    }
+}
